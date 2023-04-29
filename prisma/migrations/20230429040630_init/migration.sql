ffb7a8444914d8ec670ec10a20b3e9fe59bfca5b
Mehar Gaur <hello@mehar.ca>
2023-04-29 00:09:05 -0400

    Initial migration for Prisma schema

---
+++ prisma/migrations/20230429040630_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "movieName" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "directorName" TEXT NOT NULL,
+    "releaseDate" BOOLEAN NOT NULL DEFAULT false,
+    "creatorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Movie" ADD CONSTRAINT "Movie_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
